22d6b8468f3875da6133f2d5ed5fbca238b283f8
Shashank Mahesh <shashank@wirelessprv-10-192-64-125.near.illinois.edu>
2020-01-22 15:57:58 -0600

    finished up to projects page

--- src/components/about.js
+++ src/components/about.js
@@ -6,16 +6,16 @@ class About extends Component {
         return (
             <div className="about">
                 <img
-                    src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRRmVz9MUKb9npDPEXYWAsSzpxxGiChwHXUs055ZDhIlK76B0zamQ&s"
+                    src="https://webstockreview.net/images/male-clipart-masculine-16.png"
                     alt="Shashank"
                     className="shashank-pic"
                 />
             
                 <div className="my-header">
-                    <h1 className="name">SHASHANK&nbsp;&nbsp;MAHESH</h1>
-                    <h4 className="university">University of Illinois at Urbana-Champaign</h4>
+                    <h1 className="name"><font>SHASHANK&nbsp;&nbsp;MAHESH</font></h1>
+                    <h4 className="university"><font>University of Illinois at Urbana-Champaign</font></h4>
                     <hr />
-                    <h4 className="skills">Java &nbsp;|&nbsp; Python &nbsp;|&nbsp; C &nbsp;|&nbsp; SQL &nbsp;|&nbsp; HTML / CSS &nbsp;|&nbsp; ReactJS &nbsp;|&nbsp; Swift</h4>
+                    <h4 className="skills"><font>Java &nbsp;|&nbsp; Python &nbsp;|&nbsp; C &nbsp;|&nbsp; SQL &nbsp;|&nbsp; HTML / CSS &nbsp;|&nbsp; ReactJS &nbsp;|&nbsp; Swift</font></h4>
                     
                     <div className="social-links">
                         <a href="https://www.linkedin.com/in/shashank-mahesh-5a7708193/"
--- src/components/projects.js
+++ src/components/projects.js
@@ -1,16 +1,66 @@
 import React, { Component } from 'react';
-import SingleProject from './singleProject';
+import { Grid, Card, CardTitle, CardText, CardActions, Button, Cell } from 'react-mdl';
 
 class Projects extends Component {
 
     render() {
         return (
             <div className="projects">
-                <h1>projects</h1>
-                <SingleProject />
-                <SingleProject />
-                <SingleProject />
+
+                <div>
+                    <h1 className="projects-title"><font>Projects</font></h1>
+                </div>
+
+                <div style={{margin: '5em'}}></div>
+
+                <Grid>
+                    <Cell col={12}>
+                        <div className="projects-grid">
+                            {this.genCard('url("sifa-app.jpg") center / cover', "SIFA Android app",
+                            "Designed an Android app for SIFA (South India Fine Arts); webiste-southindiafinearts.org",
+                            "https://github.com/Shashank-Mahesh/SIFA-app")}
+
+                            {this.genCard('url("emergenspeech.png") center / cover', "Emergenspeech",
+                            "An Android app that uses speech-recognition to provide medical help for someone who has just had a medical emergency before Medical help arrives.",
+                            "https://github.com/Shashank-Mahesh/Emergenspeech")}
+
+                            {this.genCard('url("factorfiction.png") center / cover', "Fact or Fiction?",
+                            "An Android app that uses AI and other algorithms to help users determine the credibility of a news source.",
+                            "https://github.com/Shashank-Mahesh/Emergenspeech")}
+                        </div>
+                    </Cell>
+                    <Cell>
+                        <div style={{margin: '4em'}}></div>
+                    </Cell>
+                    <Cell col={12}>
+                        <div className="projects-grid">
+                            {this.genCard('url("towerdefense.jpg") center / cover', "Tower Defense 2.0",
+                            "A game much like Bloons Tower Defense reddesigned in JavaFX",
+                            "https://github.com/Shashank-Mahesh/TowerDefense")}
+
+                            {this.genCard('url("automation.jpg") center / cover', "Home Automation Model",
+                            "Developed a home automation system to control various home appliances using machine learning algorithms. Used a Raspberry Pi to model home appliances.",
+                            "/site")}
+                        </div>
+                    </Cell>
+                </Grid>
             </div>
+        )
+    }
+
+    genCard(src, projName, txt, link) {
+        return (
+            <Card shadow={5} style={{minWidth: '320px', margin: 'auto'}}>
+                <CardTitle style={{background: src, color: 'black', height: '350px'}}>
+                </CardTitle>
+                <CardText style={{height: '150px'}}>
+                    <h4 style={{color: 'black', fontFamily:'verdana'}}>{projName}</h4>
+                    {txt}
+                </CardText>
+                <CardActions border>
+                    <Button colored target="_blank" href={link}>GitHub</Button>
+                </CardActions>
+            </Card>
         )
     }
 

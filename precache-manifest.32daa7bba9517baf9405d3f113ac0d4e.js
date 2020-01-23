94a230f521c68d061beffc09a822932258906a11
Shashank Mahesh <shashank@wirelessprv-10-192-64-125.near.illinois.edu>
2020-01-23 16:11:35 -0600

    Updates

--- precache-manifest.32daa7bba9517baf9405d3f113ac0d4e.js
+++ precache-manifest.32daa7bba9517baf9405d3f113ac0d4e.js
@@ -1,6 +1,6 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "5cd5b7e8ebc987572c044901304b32d9",
+    "revision": "176f9c6b88157b7b2758672b6e104449",
     "url": "/site/index.html"
   },
   {
@@ -8,8 +8,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/site/static/css/2.a6bc1c31.chunk.css"
   },
   {
-    "revision": "0bf0a0fe6a95c6c36880",
-    "url": "/site/static/css/main.a84d2e36.chunk.css"
+    "revision": "e15f5edd838ec4b33cab",
+    "url": "/site/static/css/main.e5a873f7.chunk.css"
   },
   {
     "revision": "f9976b95d49d95fbdadc",
@@ -20,8 +20,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/site/static/js/2.450c4baa.chunk.js.LICENSE"
   },
   {
-    "revision": "0bf0a0fe6a95c6c36880",
-    "url": "/site/static/js/main.15a7963f.chunk.js"
+    "revision": "e15f5edd838ec4b33cab",
+    "url": "/site/static/js/main.34a6d204.chunk.js"
   },
   {
     "revision": "0e68fd62559e44335449",

4f1d4e0308e34b240babe92bc9ec5032213d8912
Shashank Mahesh <shashank@Shashanks-MacBook-Pro.local>
2020-05-30 22:10:40 -0700

    updated small error

--- src/components/about.js
+++ src/components/about.js
@@ -52,11 +52,6 @@ class About extends Component {
                     <p><font>
                         Hi, I'm Shashank!
                         I am a freshman at UIUC studying computer engineering, and am fast-tracked to graduate in 3 years.
-                        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore
-                        et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi
-                        ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
-                        cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa
-                        qui officia deserunt mollit anim id est laborum.
                     </font></p>
                 </div>
             </div>            
